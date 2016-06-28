0d532147c5e9cdafe0aff6651ca50c1692d04c1c
daocode <sevkav.ufo@gmail.com>
2016-06-28 12:25:01 +0300

    Top complete

--- app/src/main/java/com/sofdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/sofdesign/devintensive/ui/activities/MainActivity.java
@@ -2,14 +2,62 @@ package com.sofdesign.devintensive.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.sofdesign.devintensive.R;
+import com.sofdesign.devintensive.utils.ConstantManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = ConstantManager.TAG_PREFIX+"Main Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        if (savedInstanceState == null) {
+            //активити запускается впервые
+        } else {
+            // активити уже создавалось
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 }
